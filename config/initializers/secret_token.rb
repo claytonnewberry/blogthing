af8b4133435d040ff5f4b58313f8b38752d05123
Clatron Newberry <newberry.clayton@gmail.com>
2013-12-02 06:22:50 +0000

    created the status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '7d336e379d290d39e9f7a4663bf9c3284702385a373f44433f1e0e286d15163dd4fc2e39daf1797a7d83723985daa6450c481b9675df784a6519cd59b1b8024a'
